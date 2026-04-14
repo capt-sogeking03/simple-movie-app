a32e1e5ef8344a256e33f37198003cc99cc478d0
capt-sogeking03 <capt.sogeking03@gmail.com>
2026-04-14 16:50:50 +0800

    second commit: Add detail screen of movies. Still need improvement, the issue: when we navigate to a movie detail screen after doing it once before, the detail screen simply display previous movie data for several millisecond and then display current movie data.

--- script.js
+++ script.js
@@ -16,52 +16,114 @@ async function getMovies() {
 
     createMoviesGrid(movies);
 
-    console.log(responses);
+    console.log(movies);
   } catch (err) {
     console.error("Error", err);
   }
 }
 
 function createMoviesGrid(movies) {
-  const movieContainer = document.getElementById("movie-container");
+  const movieContainer = document.querySelector("#movie-container");
 
   movieContainer.replaceChildren();
 
-  movies.forEach(async (movie) => {
+  // create movie card
+  movies.forEach((movie) => {
     const movieMetadata = {
+      id: movie.imdbID,
       title: movie.Title,
       year: movie.Year,
       rating: movie.imdbRating,
       poster: movie.Poster,
     };
 
-    const card = await createMovieCard(movieMetadata);
-
-    // console.log(movie.Poster);
+    const card = createMovieCard(movieMetadata);
 
     movieContainer.append(card);
   });
+
+  // add event for detail screen
+  movieContainer.addEventListener("click", (e) => navigateToDetails(e));
 }
 
-async function createMovieCard(metadata) {
-  const article = document.createElement("article");
-  const img = document.createElement("img");
-  const pTitle = document.createElement("p");
-  const pYear = document.createElement("p");
-  const pRating = document.createElement("p");
-  const div = document.createElement("div");
+const navigateToDetails = async (e) => {
+  const container = document.querySelector("#movie-container");
+  // const root = document.querySelector("#root");
 
-  pTitle.textContent = metadata.title;
-  pYear.textContent = metadata.year;
-  pRating.textContent = metadata.rating;
-  img.src = metadata.poster;
+  // pick specific movie card
+  const currentCard = e.target.closest(".c-movie-card");
+  if (currentCard && container.contains(currentCard)) {
+    const detailScreen = document.querySelector("#movie-detail-screen");
 
-  div.append(pYear, pRating);
+    detailScreen.classList.remove("u-display--none");
 
-  article.append(img, pTitle, div);
-  article.classList.add("c-movie-card");
+    const movieId = currentCard.dataset.id;
 
-  return article;
+    const movie = await getMovieDetail(movieId);
+
+    console.log(movie);
+
+    renderMovieDetail(detailScreen, movie);
+
+    // When entering detail screen
+    history.pushState({ page: "details", id: movieId }, "", `#/movie/${movieId}`);
+  }
+};
+
+function renderMovieDetail(detailScreen, movie) {
+  detailScreen.innerHTML = `
+      <div class="c-detail--container">
+          <img src=${movie.Poster} alt="img" />
+          <div class="c-detail--highlight">
+            <p class="movie-detail--title">${movie.Title}</p>
+            <p class="movie-detail--year">${movie.Year}</p>
+            <p class="movie-detail--rating">${movie.imdbRating}</p>
+          </div>
+        </div>
+    `;
+
+  detailScreen.addEventListener("click", (e) => {
+    if (!e.target.closest("img")) {
+      window.history.back();
+    }
+  });
 }
+
+const getMovieDetail = async (id) => {
+  try {
+    const url = `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`;
+    const response = await fetch(url);
+    const movie = await response.json();
+
+    return movie;
+  } catch (err) {
+    console.error("failed to fetch movie\n", err);
+  }
+};
+
+function createMovieCard(metadata) {
+  const card = document.createElement("article");
+
+  card.setAttribute("data-id", metadata.id);
+  card.innerHTML = `
+    <img src="${metadata.poster}" alt="${metadata.title}">
+    <p>${metadata.title}</p>
+    <div>
+      <p>${metadata.year}</p>
+      <p>${metadata.rating}</p>
+    </div>
+  `;
+  card.classList.add("c-movie-card");
+
+  return card;
+}
+
+window.addEventListener("popstate", (event) => {
+  if (!event.state || event.state.page !== "details") {
+    const detailScreen = document.querySelector("#movie-detail-screen");
+
+    detailScreen.classList.add("u-display--none");
+  }
+});
 
 getMovies();
